941c7562e3ef031fbc78fd2a4088b54b699cb40b
Diego Auyón <diegoeauyonh@gmail.com>
2024-11-10 22:53:14 -0600

    Init project with new templates and styles

---
+++ ckanext/marn_theme/blueprint.py
@@ -0,0 +1,8 @@
+# encoding: utf-8
+
+import ckan.lib.base as base
+import ckan.lib.helpers as helpers
+from flask import Blueprint
+render = base.render
+
+marn_theme = Blueprint(u'marn_theme', __name__)
